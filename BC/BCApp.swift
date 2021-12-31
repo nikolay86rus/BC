f589a88c458ef718410edaa9542506389184dec3
Nikolay Sabadash <nikaoly86rus@gmail.com>
2021-12-31 20:01:43 +0300

    Initial Commit

---
+++ BC/BCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BCApp.swift
+//  BC
+//
+//  Created by Nikolay on 31.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BC
+//
+//  Created by Nikolay on 31.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
